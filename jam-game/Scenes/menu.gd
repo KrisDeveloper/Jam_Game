c82764a22ffefee617d003adc6f0a353f43952f8
KrisDeveloper <c730746@gmail.com>
2025-11-11 17:39:22 +0100

    designing the main menu

--- jam-game/Scenes/menu.gd
+++ jam-game/Scenes/menu.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+@onready var splash_controller = $Splash_Controller
+@onready var main_menu_container = $Main_Menu_Container
+
+func _ready() -> void:
+	main_menu_container.position.y = 100000
+	splash_controller.position.y = 0
+	await get_tree().create_timer(2).timeout
+	splash_controller.position.y = 100000
+	await get_tree().create_timer(0.5).timeout
+	main_menu_container.position.y = 0
---
+++ jam-game/Scripts/action_button.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+var default_color = Color8(255, 255, 47)
+
+func _ready() -> void:
+	self.modulate = default_color
