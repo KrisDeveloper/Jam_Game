f187eb741a0042636d90d3c08e4187aa17afa264
KrisDeveloper <c730746@gmail.com>
2025-11-11 23:36:20 +0100

    reimporting icons

--- jam-game/Scripts/action_button.gd
+++ jam-game/Scripts/action_button.gd
@@ -1,6 +1,20 @@
 extends Node2D
 
+@export var icon : Sprite2D
+@onready var label = $Label
+@onready var button = $Button
+
 var default_color = Color8(255, 255, 47)
 
-func _ready() -> void:
-	self.modulate = default_color
+var is_disabled : bool
+
+func _process(delta: float) -> void:
+	if button.is_hovered():
+		icon.modulate = Color.WHITE
+		label.modulate = Color.WHITE
+	elif is_disabled:
+		icon.modulate = Color.RED
+		label.modulate = Color.RED
+	else:
+		icon.modulate = default_color
+		label.modulate = default_color
